665bf3513014d22945ea3dd3d8aada483ceda352
Thakur Yogeshwar Singh <thakuryogeshsingh@gmail.com>
2020-12-30 15:31:43 +0530

    fix: service call handling fixed

--- geo-location/src/Components/Dashboard/Dashboard.jsx
+++ geo-location/src/Components/Dashboard/Dashboard.jsx
@@ -12,59 +12,30 @@ const Dashboard = () => {
     reader.readAsText(files[0]);
   };
 
-  /* function* fetchData(arr, url) {
-    let placesData = [];
-    arr.forEach(async (item) => {
-      await fetch(
-        `${url}&mode=retrieveAddresses&prox=${item.Latitude},${item.Longitude}`
-      )
-        .then((response) => response.json())
-        .then((res) => {
-          if (!res.error) {
-            const address =
-              res?.Response?.View[0]?.Result[0]?.Location?.Address?.Label;
-            placesData.push({
-              ...item,
-              address,
-            });
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-          placesData.push({ err });
-        });
-    });
-    console.log("placesData", placesData);
-    yield placesData;
-  } */
-
   const onReaderLoad = (event) => {
     const arr = JSON.parse(event.target.result);
     const key = "9RI-8p6SV3TxPBBKPa81H680SFQl80d8Txr2AyXq4tU";
     const url = `https://reverse.geocoder.ls.hereapi.com/6.2/reversegeocode.json?apiKey=${key}`;
     let placesData = [];
-    arr.forEach(async (item) => {
-      await fetch(
-        `${url}&mode=retrieveAddresses&prox=${item.Latitude},${item.Longitude}&limit=1`
-      )
-        .then((response) => response.json())
-        .then((res) => {
-          if (!res.error) {
-            const address =
-              res?.Response?.View[0]?.Result[0]?.Location?.Address?.Label;
-            placesData.push({
-              ...item,
-              address,
-            });
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-          placesData.push({ err });
-        });
+    const promises = arr.map((item) => {
+      return fetch(
+        `${url}&mode=retrieveAddresses&prox=${item.Latitude},${item.Longitude}`
+      ).then((response) => {
+        return response.json();
+      });
     });
-    console.log("placesData", placesData);
-    setLocations(placesData);
+    Promise.all(promises).then((results) => {
+      console.log(results);
+      results.forEach((res, index) => {
+        const address =
+          res?.Response?.View[0]?.Result[0]?.Location?.Address?.Label;
+        placesData.push({
+          ...arr[index],
+          address,
+        });
+      });
+      setLocations(placesData);
+    });
   };
 
   return (
